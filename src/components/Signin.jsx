085b1dd88ecd8667c41d13a3976d41af8072345c
younes789 <younes.666.ayouj@gmail.com>
2022-05-05 19:09:14 +0000

    k

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -49,7 +49,7 @@ const Signin = () => {
           ></input>
         </div>
         <button className="border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white">
-          Sign up
+          Sign in
         </button>
       </form>
     </div>
